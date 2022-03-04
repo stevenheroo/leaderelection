22a0b75e300ff20da41a2bc01932711d004c3cdb
stevenheroo <stevenfianu99@gmail.com>
2022-03-04 07:12:03 +0000

    leader electing algorithm

--- README.md
+++ README.md
@@ -1,15 +1,15 @@
 # leaderelection
 
-A simple distributed system logic using Zookeeper to simplify selection of znodes, 
+A simple distributed system logic using Zookeeper to simplify selection of znodes,
 
 # Setup Apache Zookeeper #
     #you need JDK 1.8 or later installed on machine first ..
 Step
-    1. follow link https://zookeeper.apache.org/releases.html to download
-    2. unzip or unrar, paste in a preferred location 
-    3. i) locate conf folder & change zoo_sample.cfg file to -> zoo.cfg
-        ii) add 
-    4. open terminal >>> in bin location and run ./zkService start 
-        (logs folder will be created automatically in zookeeper folder)
-    5. update dataDir in zoo.cfg file ->>>> eg.(mine) dataDir=home/***/zookeeper/logs
-**bin dir <> contains CL tools for zookeeper**
+1. follow link https://zookeeper.apache.org/releases.html to download
+2. unzip or unrar, paste in a preferred location
+3. locate conf folder & change zoo_sample.cfg file to -> zoo.cfg
+4. open terminal >>> in bin location and run ./zkService start
+(logs folder will be created automatically in zookeeper folder)
+5. update dataDir in zoo.cfg file ->>>> eg.(mine) dataDir=home/***/zookeeper/logs
+
+--bin dir <> contains CL tools for zookeeper
--- src/main/java/LeaderElection.java
+++ src/main/java/LeaderElection.java
@@ -1,20 +1,48 @@
-import org.apache.zookeeper.WatchedEvent;
-import org.apache.zookeeper.Watcher;
-import org.apache.zookeeper.ZooKeeper;
+import org.apache.zookeeper.*;
 
 import java.io.IOException;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
 
 public class LeaderElection implements Watcher {
 
     private static final String ZOOKEEPER_ADDRESS = "localhost:2181";
     private static final int SESSION_TIMEOUT = 3000;
+    private static final String ELECTION_NAMESPACE = "/election";
+    private String currentZnodeName;
     private ZooKeeper zooKeeper;
 
-    public static void main(String [] args) throws IOException, InterruptedException {
+    public static void main(String [] args) throws IOException, InterruptedException, KeeperException {
         LeaderElection election = new LeaderElection();
         election.connectToZookeeper();
+        election.volunteerForLeadership();
         election.run();
         election.close();
+    }
+
+    //create a volunteer nodes for selection
+    public void volunteerForLeadership() throws InterruptedException, KeeperException {
+        String znodePrefix = ELECTION_NAMESPACE + "/c_";
+        String znodeFullPath = zooKeeper.create(znodePrefix, new byte[] {}, ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL_SEQUENTIAL);
+        System.out.println("znode-->>> "+ znodeFullPath);
+
+        this.currentZnodeName = znodeFullPath.replace(ELECTION_NAMESPACE + "/", "");
+        electLeader();
+    }
+
+    //elect leader process
+    public void electLeader() throws InterruptedException, KeeperException {
+        //store all created nodes in a list
+        List<String> zChildren = zooKeeper.getChildren(ELECTION_NAMESPACE, false);
+        //check for the smallest index in array and store
+        String smallestChild = zChildren.get(0);
+
+        if (smallestChild.equalsIgnoreCase(currentZnodeName))
+            System.out.println("I am the Leader");
+        else
+            System.out.println("I'm not Leader!! " + currentZnodeName + "is the leader");
+
     }
 
     public void connectToZookeeper() throws IOException {
